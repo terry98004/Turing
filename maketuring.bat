7d5e3c77b6c4779f479366f3cbf656f3a20e3958
Terry Murphy <vendor@tpmassoc.com>
2026-01-11 13:24:51 -0800

    Correct the static flag

--- maketuring.bat
+++ maketuring.bat
@@ -1,3 +1,3 @@
 gcc -Wall -Wextra -pedantic-errors -std=gnu17 -pthread -c turing.c 
 gcc -Wall -Wextra -pedantic-errors -std=gnu17 -pthread -c CompTuring.c 
-gcc --static -pthread -o turing turing.o CompTuring.o  -L. -l:libhgt.a -l:libmpfr.a -l:libgmp.a
+gcc -static -pthread -o turing turing.o CompTuring.o  -L. -l:libhgt.a -l:libmpfr.a -l:libgmp.a
